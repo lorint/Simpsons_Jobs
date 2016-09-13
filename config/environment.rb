048401a939fc105ebfe0ad2e00a6ed0a1abda55b
lorint <lorint@gmail.com>
2016-09-13 11:06:32 +0100

    Accidentally had a require to user.rb still around

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,5 @@
 
 require 'standalone_migrations'
 require './models/person'
-require './models/user'
 
 ActiveRecord::Base.establish_connection YAML.load_file('db/config.yml')[ENV["RAILS_ENV"] || ENV["RACK_ENV"] || "development"]
